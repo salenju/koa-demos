4111cc6c03cbbc03457c1ed217f02793aa5be79f
salen <salenju@163.com>
2019-06-19 22:16:28 +0800

    路由参数

--- app.js
+++ app.js
@@ -3,34 +3,53 @@ const json = require('koa-json');
 const KoaRouter = require('koa-router');
 const path = require('path');
 const render = require('koa-ejs');  // 模板引擎
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRouter();
 
-// 格式换json数据
-app.use(json());
+app.use(json());  // 格式换json数据
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());  // 配置路由模块
 
 // Mock数据
-let things  = ['salen','hellen','lemmon'];
+let things = ['salen', 'hellen', 'lemmon'];
 
-// 配置路由模块
-app.use(router.routes()).use(router.allowedMethods())
-
-// 配置模板殷引擎
-render(app,{
-  root:path.join(__dirname,'views'),  // __dirname为app.js的路径
-  layout:'layout',
-  viewExt:'html',
-  cache:false,
-  debug:false
+// 配置模板引擎
+render(app, {
+  root: path.join(__dirname, 'views'),  // __dirname为app.js的路径
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
 });
 
-// 路由跳转 index
-router.get('/index',async ctx => {
-  await ctx.render('index',{
-    title:'Hello Salen...',
-    things:things
+// 路由跳转 
+router.get('/', index);
+
+async function index(ctx) {
+  await ctx.render('index', {
+    title: 'Hello Salen...',
+    things: things
   });
-});
+}
+
+// 路由跳转 "/add" 
+router.get('/add', showAdd);
+
+async function showAdd(ctx) {
+  await ctx.render('add');
+}
+
+// 添加路由方法
+router.post('/add', add);
+
+async function add(ctx) {
+  const body = ctx.request.body;
+  things.push(body.thing);
+  ctx.redirect('/');
+}
+
+router.get('/test/:id', ctx => (ctx.body = `Hello ${ctx.params.id}`))
 
 app.listen(3000, () => console.log('Start...'));
